8811eb651bb8ae47e0c3aebc8d125c6c41c106ab
Nihar Devineni <nihar2570@gmail.com>
2023-05-30 17:40:45 +0530

    minor comment edit

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:nihar2570/nihar2570.github.io.git main
+#git push -f git@github.com:nihar2570/nihar2570.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:nihar2570/meet.git main:gh-pages
