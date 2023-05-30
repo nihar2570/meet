74ccf0404115fab170101f1011e71ba48efe7b3a
Nihar Devineni <nihar2570@gmail.com>
2023-05-30 19:27:00 +0530

    unbreak

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
 import ProfileView from "../views/ProfileView.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
